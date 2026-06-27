35046e45a6b578cac45ecef29b80d3e17ef5b205
Ashutosh Das <dasashutosh606@gmail.com>
2026-06-27 18:03:15 +0530

    Add Docker support for alert service

--- services/alert-service/src/services/slackService.js
+++ services/alert-service/src/services/slackService.js
@@ -1,6 +1,6 @@
 const { IncomingWebhook } = require("@slack/webhook");
 require("dotenv").config();
-console.log(process.env.SLACK_WEBHOOK_URL);
+//console.log(process.env.SLACK_WEBHOOK_URL);
 const webhook = new IncomingWebhook(
   process.env.SLACK_WEBHOOK_URL
 );
